b3cdd193374fcb5949ec062a98df717da42d3df3
kholodovvv <apersand@mail.ru>
2021-10-16 00:41:13 +1000

    Home Work For Lesson 6

---
+++ lesson7/cmake-build-debug/CMakeFiles/lesson7.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/lesson7.dir/main.cpp.obj"
+  "lesson7.exe"
+  "lesson7.exe.manifest"
+  "lesson7.pdb"
+  "liblesson7.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/lesson7.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ lesson7/main.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+#include "test.pb.h"
+
+int main() {
+
+    Car car;
+
+    return 0;
+}
